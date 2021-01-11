55fa948766c64efd613100f7c022adb5713ee559
Thijmen <thijmenh01@gmail.com>
2021-01-11 17:05:40 +0100

    spawner 4/10 af

---
+++ Assets/Scripts/Thijmen Scripts/Damagable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagable : MonoBehaviour
+{
+    public interface IDamagable
+    {
+        void TakeHit(float damage, RaycastHit hit);
+    }
+}
---
+++ Assets/Scripts/Thijmen Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Boat boat;
+    [SerializeField] private GameObject boatObj;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
