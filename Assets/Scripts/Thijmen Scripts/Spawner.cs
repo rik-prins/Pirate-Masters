46f98a51c3317ba18c28edd809c55c153329c86b
Thijmen <thijmenh01@gmail.com>
2021-01-19 18:07:59 +0100

    Spawner fin.

--- Assets/Scripts/Thijmen Scripts/Spawner.cs
+++ Assets/Scripts/Thijmen Scripts/Spawner.cs
@@ -8,59 +8,37 @@ public class Spawner : MonoBehaviour
     [SerializeField] private GameObject boatObj;
 
     [SerializeField] private int spawnAmount = 3;
-    [SerializeField] private float[] spawnMoment;
     private float spawninterval;
     private float Spawnpoint;
     private float Randomspawnpoint;
 
     private void Start()
     {
-        spawnMoment = new float[spawnAmount];
         Spawnpoint = (boat.distance / 3);
         spawninterval = (boat.distance / 3);
 
         Randomspawnpoint = Random.Range(Spawnpoint - 1, Spawnpoint + 1);
 
         print(Randomspawnpoint);
-        //CalculateSpawnMoments();
     }
 
-    //public void CalculateSpawnMoments()
-    //{
-    //    for (int i = 0; i < spawnAmount; i++)
-    //    {
-    //        spawnMoment[i] = Random.Range(0, boat.distance);
-
-    //        print(boat.distance);
-    //        print(spawnMoment);
-    //    }
-    //}
-
+    
     private void Update()
     {
-        //foreach (float moment in spawnMoment)
-        //{
-        //    if (Mathf.Abs(moment - boat.progress) <= 0.0001f)
-        //    {
-        //        print("spawn");
-        //    }
-        //}
-
-        SpawnNiggas();
+        CalculateSpawnpoint();
     }
 
-    private void SpawnNiggas()
+    private void CalculateSpawnpoint()
     {
         if (Mathf.Abs(Randomspawnpoint - boat.progress) <= 0.001f)
         {
-            //Spawn Boat
-            SpawnRealNiggas();
+            SpawnEnemy();
         }
     }
 
-    private void SpawnRealNiggas()
+    private void SpawnEnemy()
     {
-        print("spawn");
+        Instantiate(boatObj, transform.position, Quaternion.identity);
         Spawnpoint += spawninterval;
         print(Spawnpoint);
         Randomspawnpoint = Random.Range(Spawnpoint - 1f, Spawnpoint);
