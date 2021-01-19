a326f5c9c244b70178c48e6d539c81eeb65d9c5c
Rik <rik-prins@hotmail.com>
2021-01-19 18:57:24 +0100

    skybox effect

---
+++ Assets/Scripts/Rik Scripts/WeatherRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
+
+public class WeatherRotation : MonoBehaviour
+{
+    private VolumeProfile volume;
+
+    private void Update()
+    {
+        volume = GetComponent<Volume>().profile;
+        if (!volume.TryGet<HDRISky>(out var sky))
+        {
+            sky = volume.Add<HDRISky>(false);
+        }
+
+        sky.rotation.value += 0.015f;
+
+        //fog.enabled.overrideState = overrideFog;
+        //fog.enabled.value = enableFog;
+    }
+}
